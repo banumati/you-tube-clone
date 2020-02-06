43dd44d1eab36642d54eb447d4ba264a515b9a3c
Banumathy M <banu@etekchnoservices.com>
2020-02-06 14:56:29 +0530

    you tube clone created

--- src/app/comment-sec/comment-sec.component.ts
+++ src/app/comment-sec/comment-sec.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentSecComponent implements OnInit {
 
+  public array = [
+    {
+      guid: '900ea552-ef68-42cc-b6a6-b8c4dff10fb7',
+      age: 32,
+      name: 'Powers Schneider',
+    },
+    {
+      guid: '880381d3-8dca-4aed-b207-b3b4e575a15f',
+      age: 25,
+      name: 'Adrian Lawrence',
+    },
+    {
+      
+      guid: '87b47684-c465-4c51-8c88-3f1a1aa2671b',
+      age: 32,
+      name: 'Boyer Stanley',
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/video-container/video-container.component.ts
+++ src/app/video-container/video-container.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoContainerComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
